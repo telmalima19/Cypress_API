ba887e85b194a6a57ef6d8f6f6b0ca451768e520
Telma Lima <limalancaster@gmail.com>
2025-06-25 19:51:56 -0300

    Login

--- cypress/e2e/automacaoApi.cy.js
+++ cypress/e2e/automacaoApi.cy.js
@@ -1,16 +1,16 @@
 describe('Teste API', () => {
-  it('Criar Usuarios', () => {
-    cy.fixture('usuario').then(function(usuario){ 
+  it('Criar Usuário', () => {
+    cy.fixture('usuario').then(function(usuario) {
       const user = usuario.body_usuario
-    cy.criar_usuario(user)
-        }).then((response)=>{
-      expect(response.status).to.eq(201)
-      expect (response.body.message).to.eq('Cadastro realizado com sucesso')
-      Cypress.env('id',response.body._id)
-   })     
-
+      cy.criar_usuario(user).then((response) => {
+        expect(response.status).to.eq(201)
+        expect(response.body.message).to.eq('Cadastro realizado com sucesso')
+        Cypress.env('id', response.body._id)
+      })
+    })
   })
-  it('Buscar Usuario', () => {
+
+  it('Busca Usuário po ID', () => {
    const id = Cypress.env('id')
     cy.buscar_usuario(id)
       .then((response)=>{
@@ -18,7 +18,7 @@ describe('Teste API', () => {
         
   })
   })
-  it('Listar Usuario', () => {
+  it('Lista Usuário', () => {
       const id = ''
     cy.buscar_usuario(id)
       .then((response)=>{
@@ -26,7 +26,7 @@ describe('Teste API', () => {
 
   })
   })
-  it('Atualizar Usuario', () => {
+  it('Atualiza Usuário', () => {
     cy.fixture('usuario').then(function(usuario){ 
       const atualiza = usuario.atualizar_usuario
     cy.criar_usuario(atualiza)         
@@ -36,7 +36,24 @@ describe('Teste API', () => {
 
   })
   })
-  it('Excluir Usuario', () => {
+
+    it('Realiza login no sistema', () => {
+    cy.fixture('usuario').then(function(usuario) {
+      const login = usuario.atualizar_usuario.email
+      const senha = usuario.atualizar_usuario.password
+      cy.api({
+      method:'POST',
+      url:'https://serverest.dev/login',
+      body: {email: login, password: senha
+      }}).then((response) => {
+        expect(response.status).to.eq(200)
+        expect(response.body.message).to.eq('Login realizado com sucesso')
+        Cypress.env('token', response.body.autorization)
+      
+    })
+  })
+  })
+  it('Exclui Usuário', () => {
     cy.api({
       method:'DELETE',
       url:`https://serverest.dev/usuarios/${Cypress.env('id')}`,
@@ -48,4 +65,5 @@ describe('Teste API', () => {
 
   })
   })
- })
+ }) 
+  
